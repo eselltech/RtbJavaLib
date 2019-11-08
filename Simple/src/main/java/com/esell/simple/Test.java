f15e52b70d8de16497c289bb440030f14295bfab
niulei <niulei@eselltech.com>
2019-11-08 18:09:47 +0800

    1、更新

--- Simple/src/main/java/com/esell/simple/Test.java
+++ Simple/src/main/java/com/esell/simple/Test.java
@@ -16,14 +16,13 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         RtbManager rtbManager = RtbManager.getInstance();
-        rtbManager.init("vazazjhenhc5psfz", "71vr6anlbg2f9lvhbbhgj8fy79b55xuc", "419102906489");
+        rtbManager.init("pxbAppId", "pxbAppKey", "unicode");
         rtbManager.request(new OnAdListener() {
             @Override
             public void onAd(Message message, List<RtbAD> adList) {
                 YLog.d(message + "," + adList);
             }
-        }, new RtbSlot("25075489", "IMG", 1), new RtbSlot("25075489", "IMG", 1), new RtbSlot(
-                "25075489", "IMG", 1));
+        }, new RtbSlot("广告位id", "类型", 1/*数量*/));
 
     }
 }
